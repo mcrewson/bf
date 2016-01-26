895f611287326780eaccb103a649b9c5091fb573
Mark Crewson <mark@crewson.net>
2016-01-25 19:27:39 -0800

    refactoring db tables

--- src/main/java/blackflag/Application.java
+++ src/main/java/blackflag/Application.java
@@ -5,12 +5,14 @@ package blackflag;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.PropertySource;
 
+@Configuration
 @SpringBootApplication
 @ImportResource("/applicationContext.xml")
-//@PropertySource("/application.properties")
+@PropertySource("/application.properties")
 public class Application
 {
     public static void main(String[] args)
--- src/main/resources/blackflag/data/migration/V0_1__start.sql
+++ src/main/resources/blackflag/data/migration/V0_1__start.sql
@@ -3,23 +3,6 @@
 
 PRAGMA foreign_keys = ON;
 
-CREATE TABLE person (
-  id          INTEGER PRIMARY KEY ASC,
-  name        TEXT,
-  sorted_name TEXT
-);
-CREATE INDEX person_idx ON person(name);
-
-
-CREATE TABLE work (
-  id        INTEGER PRIMARY KEY ASC,
-  name      TEXT,
-  media_type INTEGER,
-
-  FOREIGN KEY(media_type) REFERENCES media_types(id)
-);
-
-
 CREATE TABLE media_types (
   id          INTEGER PRIMARY KEY ASC,
   parent_type INTEGER,
@@ -53,9 +36,52 @@ INSERT INTO media_types (id,parent_type,name) values(19,5,"movie");
 INSERT INTO media_types (id,parent_type,name) values(20,5,"tv episode");
 
 
+CREATE TABLE metadata (
+  id INTEGER PRIMARY KEY ASC,
+  key TEXT,
+  value TEXT
+);
+CREATE INDEX metadata_idx ON metadata(key);
+
+
+CREATE TABLE people (
+  id          INTEGER PRIMARY KEY ASC,
+  name        TEXT,
+  sorted_name TEXT
+);
+CREATE INDEX people_idx ON people(name);
+
+CREATE TABLE people_metadata (
+  person_id  INTEGER REFERENCES people(id),
+  metadata_id INTEGER REFERENCES metadata(id),
+
+  PRIMARY KEY(person_id,metadata_id)
+);
+
+
+CREATE TABLE works (
+  id        INTEGER PRIMARY KEY ASC,
+  name      TEXT,
+  media_type INTEGER,
+
+  FOREIGN KEY(media_type) REFERENCES media_types(id)
+);
+CREATE INDEX works_idx ON works(name);
+
+CREATE TABLE works_metadata (
+  work_id     INTEGER REFERENCES works(id),
+  metadata_id INTEGER REFERENCES metadata(id),
+
+  PRIMARY KEY(work_id,metadata_id)
+);
+
+
 CREATE TABLE system_messages (
   timestamp INTEGER,
   reporter  TEXT,
   content   TEXT
 );
 CREATE INDEX system_messages_idx ON system_messages(timestamp);
+
+--
+-- THE END
